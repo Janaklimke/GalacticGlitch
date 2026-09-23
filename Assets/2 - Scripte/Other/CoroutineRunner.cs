f4fdc8e5359f0e6bffba0e5bf30f40d9b61f0e3f
Janaklimke <janaklimke73@gmail.com>
2026-09-23 10:37:09 +0200

    idk atp

---
+++ Assets/2 - Scripte/Other/CoroutineRunner.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CoroutineRunner : MonoBehaviour
+{
+    private static CoroutineRunner _instance;
+    public static CoroutineRunner Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                var go = new GameObject("CoroutineRunner");
+                _instance = go.AddComponent<CoroutineRunner>();
+                Object.DontDestroyOnLoad(go);
+            }
+            return _instance;
+        }
+    }
+}
--- Assets/2 - Scripte/Specials/WormshootablleCollectible.cs
+++ Assets/2 - Scripte/Specials/WormshootablleCollectible.cs
@@ -5,61 +5,36 @@ public class WormshootablleCollectible : MonoBehaviour
 {
     [Header("Health Settings")]
     public bool randomHitsToDestroy = false;
-
-    [Tooltip("Wird nur benutzt, wenn 'Random Hits To Destroy' AUS ist")]
     public int hitsToDestroy = 3;
-
-    [Tooltip("Wird nur benutzt, wenn 'Random Hits To Destroy' AN ist")]
     public int minRandomHits = 1;
     public int maxRandomHits = 5;
 
     private int currentHits;
 
     [Header("Collectable Settings")]
-    [Tooltip("Das Canvas (oder GameObject), das beim Einsammeln kurz aktiviert wird")]
     public GameObject canvasToShow;
-
-    [Tooltip("Wie lange (in Sekunden) das Canvas sichtbar bleibt")]
     public float displayDuration = 3f;
-
-    [Tooltip("Tag des Spielers, der den Wurm einsammeln kann")]
     public string playerTag = "Player";
 
     private bool isCollected = false;
 
     void Start()
     {
-        if (randomHitsToDestroy)
-        {
-            currentHits = Random.Range(minRandomHits, maxRandomHits + 1);
-        }
-        else
-        {
-            currentHits = hitsToDestroy;
-        }
+        currentHits = randomHitsToDestroy
+            ? Random.Range(minRandomHits, maxRandomHits + 1)
+            : hitsToDestroy;
 
         if (canvasToShow != null)
-        {
             canvasToShow.SetActive(false);
-        }
     }
-
 
     public void TakeHit()
     {
         if (isCollected) return;
 
         currentHits--;
-
         if (currentHits <= 0)
-        {
-            DestroyObject();
-        }
-    }
-
-    void DestroyObject()
-    {
-        Destroy(gameObject);
+            Destroy(gameObject);
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -70,10 +45,14 @@ public class WormshootablleCollectible : MonoBehaviour
         {
             TakeHit();
             Destroy(collision.gameObject);
-            return;
         }
+    }
 
-        if (collision.CompareTag(playerTag))
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isCollected) return;
+
+        if (collision.collider.CompareTag(playerTag))
         {
             Collect();
         }
@@ -83,24 +62,26 @@ public class WormshootablleCollectible : MonoBehaviour
     {
         isCollected = true;
 
-        Collider2D col = GetComponent<Collider2D>();
-        if (col != null) col.enabled = false;
+        gameObject.SetActive(false);
 
-        SpriteRenderer sr = GetComponent<SpriteRenderer>();
-        if (sr != null) sr.enabled = false;
-
-        StartCoroutine(ShowCanvasThenDestroy());
-    }
-
-    IEnumerator ShowCanvasThenDestroy()
-    {
         if (canvasToShow != null)
         {
-            canvasToShow.SetActive(true);
-            yield return new WaitForSeconds(displayDuration);
-            canvasToShow.SetActive(false);
+            CoroutineRunner.Instance.StartCoroutine(
+                ShowCanvasThenHide(canvasToShow, displayDuration, gameObject)
+            );
         }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
-        Destroy(gameObject);
+    static IEnumerator ShowCanvasThenHide(GameObject canvas, float duration, GameObject wormToDestroy)
+    {
+        canvas.SetActive(true);
+        yield return new WaitForSeconds(duration);
+        canvas.SetActive(false);
+
+        Destroy(wormToDestroy);
     }
 }
